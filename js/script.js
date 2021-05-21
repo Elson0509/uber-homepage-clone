4448ed9acae336c1217f1f6c7c462f1d85267536
Elson Ramos <elson.0509@gmail.com>
2021-05-21 12:05:49 -0400

    depuring js, turning simpler the window handler

--- js/script.js
+++ js/script.js
@@ -1,49 +1,64 @@
 //references
-const navitoggle = document.getElementById("navi-toggle")
 const hamb = document.getElementById("hamb")
 const hamburguer = document.getElementById("hamburguer")
 const close = document.getElementById("close")
-const naviToggleProducts = document.getElementById("navi-toggle-products")
 const companyMenuButton = document.getElementById("company-menu")
-const companySubNav = document.getElementById("company-subnav")
 
-let isMenuOpen = false
-let isMenuProductsOpen = false
-let isCompanyMenuOpen = false
+const windows = [
+    'navi-toggle-products','company-subnav','language-toggle', 'navi-toggle'
+]
 
-const toggleMenu = _ => {
-    isMenuOpen = !isMenuOpen
-    if(isMenuOpen){
-        navitoggle.style.display='block'
-        hamburguer.innerHTML='<i class="fas fa-times"></i>'
+const isOpenWindows = []
+
+windows.forEach(el => {
+    isOpenWindows[el] = false
+})
+
+const openMenu = id => {
+    //if it is menu toggle and it is a X, close all.
+    if(id==='navi-toggle' && (isOpenWindows['navi-toggle'] || isOpenWindows['navi-toggle-products'])){
+        closeAllMenus()
+        return
     }
-    else{
-        isMenuProductsOpen = false
-        navitoggle.style.display='none'
-        naviToggleProducts.style.display='none'
-        hamburguer.innerHTML='<div class="hamb"></div>'
-    }
+    //iterating windows by windows to check which one was invoked
+    windows.forEach(win => {
+        const el = document.getElementById(win)
+        if(id===win){
+            //if that window has been invoked, show it
+            el.style.display=isOpenWindows[id] ? 'none' : 'block'
+            //set true to its window flag
+            isOpenWindows[id] = !isOpenWindows[id]
+            
+        }
+        else{
+            //if that window has not been invoked, close it
+            el.style.display='none'
+            //set true to its window flag
+            isOpenWindows[win] = false
+            
+        }
+        //checking if changing the icon is needed
+        if(isOpenWindows['company-subnav']){
+            companyMenuButton.innerHTML = '<a href="#">Company &nbsp&nbsp<i class="fas fa-chevron-up arrow-company"></i></a>'
+        }
+        else{
+            companyMenuButton.innerHTML = '<a href="#">Company &nbsp&nbsp<i class="fas fa-chevron-down arrow-company"></i></a>'
+        }
+        if(isOpenWindows['navi-toggle'] || isOpenWindows['navi-toggle-products']){
+            hamburguer.innerHTML='<i class="fas fa-times"></i>'
+        }
+        else{
+            hamburguer.innerHTML='<div class="hamb"></div>'
+        }
+    })
 }
 
-const productHandler = _ => {
-    isMenuProductsOpen = !isMenuProductsOpen
-    if(isMenuProductsOpen){
-        naviToggleProducts.style.display='block'
-    }
-    else{
-        naviToggleProducts.style.display='none'
-    }
-}
-
-const companyMenuHandler = _ => {
-    isCompanyMenuOpen = !isCompanyMenuOpen
-    if(isCompanyMenuOpen){
-        companyMenuButton.innerHTML = '<a href="#">Company &nbsp&nbsp<i class="fas fa-chevron-up arrow-company"></i></a>'
-        companySubNav.style.display='block'
-    }
-    else{
+const closeAllMenus = _ => {
+    windows.forEach(win => {
+        const el = document.getElementById(win)
+        el.style.display='none'
+        isOpenWindows[win] = false
         companyMenuButton.innerHTML = '<a href="#">Company &nbsp&nbsp<i class="fas fa-chevron-down arrow-company"></i></a>'
-        companySubNav.style.display='none'
-    }
-
+        hamburguer.innerHTML='<div class="hamb"></div>'
+    })
 }
